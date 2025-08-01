9d571338cdf6de7af4083999d6e028a3b186f1e8
AELD73 <angelesaulopezdiaz23@gmail.com>
2025-07-31 22:04:17 -0600

    Creación Patron DAO para prenda (interface)

---
+++ src/java/mx/uam/azc/Modelo/DAO_Prenda.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package mx.uam.azc.Modelo;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author lopez
+ */
+public interface DAO_Prenda <T>{
+    Optional<T> get(String correo, String password);
+    
+    List<T> getAll();
+    
+    void save(T t);
+    
+    void update(T t, String[] params);
+    
+    void delete(T t);
+}

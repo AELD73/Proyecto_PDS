3e0d56111af94e7a3badc5c4080d47215ef6c594
CASH <CASH@DESKTOP-5220OB9>
2025-08-03 14:39:22 -0600

    Implementacion del patron Observador para la actualización del inventario desde el carrito

--- src/java/mx/uam/azc/Modelo/Producto.java
+++ src/java/mx/uam/azc/Modelo/Producto.java
@@ -8,13 +8,15 @@ public class Producto {
     private int id;
     private String nombre;
     private double precio;
+    private int stock;
 
     public Producto() {}
 
-    public Producto(int id, String nombre, double precio) {
+    public Producto(int id, String nombre, double precio, int stock) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
+        this.stock = stock;
     }
 
     public int getId() {
@@ -40,5 +42,15 @@ public class Producto {
     public void setPrecio(double precio) {
         this.precio = precio;
     }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+    
 }
 

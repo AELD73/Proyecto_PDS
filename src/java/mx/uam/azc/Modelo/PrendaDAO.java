16c4351f8679935675b94005eaeeaed0239c3412
lopezdiaz1bm@gmail.com <lopezdiaz1bm@gmail.com>
2025-08-03 12:23:51 -0600

    Agregue comentarios para ver el funcionamiento, pero ya se quitaron

--- src/java/mx/uam/azc/Modelo/PrendaDAO.java
+++ src/java/mx/uam/azc/Modelo/PrendaDAO.java
@@ -33,42 +33,49 @@ public class PrendaDAO implements DAO_Prenda<Prenda>{
     }
 
     @Override
-    public List<Prenda> getAll() {
-        List<Prenda> lista_prendas = new ArrayList<>();
-        try {
-            
-            String sql = """
-                                     SELECT p.id_prenda, tp.nombre_prenda, tp.tipo, c.nombre_color, t.nombre_talla,
-                                            p.costo_personal, p.costo_empresarial
-                                     FROM Prenda p
-                                     JOIN TipoPrenda tp ON p.id_tipo_prenda = tp.id_tipo_prenda
-                                     JOIN Color c ON p.id_color = c.id_color
-                                     JOIN Talla t ON p.id_talla = t.id_talla
-                                 """;
-            
-            PreparedStatement stm = conexion.prepareStatement(sql);
-            ResultSet rs = stm.executeQuery();
-            
-            while(rs.next()){
-                PrendaBase prenda = new PrendaBase();
-                prenda.setId_prenda(rs.getInt("id_prenda"));
-                prenda.setTipo_prenda(rs.getString("nombre_prenda") + " - " + rs.getString("tipo"));
-                prenda.setColor_prenda(rs.getString("nombre_color"));
-                prenda.setTalla_prenda(rs.getString("nombre_talla"));
+public List<Prenda> getAll() {
+    List<Prenda> lista_prendas = new ArrayList<>();
+    try {
+        
 
-                double costo = tipo_Usr.equalsIgnoreCase("Empresarial")
-                               ? rs.getDouble("costo_empresarial")
-                               : rs.getDouble("costo_personal");
+        String sql = """
+            SELECT p.id_prenda, tp.nombre_prenda, tp.tipo, c.nombre_color, t.nombre_talla,
+                   p.costo_personal, p.costo_empresarial
+            FROM Prenda p
+            JOIN TipoPrenda tp ON p.id_tipo_prenda = tp.id_tipo_prenda
+            JOIN Color c ON p.id_color = c.id_color
+            JOIN Talla t ON p.id_talla = t.id_talla
+        """;
 
-                prenda.setCosto(costo);
-                lista_prendas.add(prenda);
-            }
+        PreparedStatement stm = conexion.prepareStatement(sql);
+        ResultSet rs = stm.executeQuery();
+
+        while (rs.next()) {
+            PrendaBase prenda = new PrendaBase();
+            prenda.setId_prenda(rs.getInt("id_prenda"));
+            prenda.setTipo_prenda(rs.getString("nombre_prenda") + " - " + rs.getString("tipo"));
+            prenda.setColor_prenda(rs.getString("nombre_color"));
+            prenda.setTalla_prenda(rs.getString("nombre_talla"));
+
+            double costo = tipo_Usr.equalsIgnoreCase("Empresarial")
+                           ? rs.getDouble("costo_empresarial")
+                           : rs.getDouble("costo_personal");
+
+            prenda.setCosto(costo);
+            lista_prendas.add(prenda);
+
             
-        } catch (SQLException ex) {
-            System.getLogger(PrendaDAO.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
-        return lista_prendas;
+
+    } catch (SQLException ex) {
+        
+        ex.printStackTrace();
     }
+
+    
+    return lista_prendas;
+}
+
 
     @Override
     public void save(Prenda t) {
